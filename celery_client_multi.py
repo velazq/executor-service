5aa8b699e2fa0e79b1513c801a70ef2d8b6c5f28
AV <netnoc@gmail.com>
2017-06-08 16:35:08 +0200

    New client that accepts a folder of folders

---
+++ celery_client_multi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import os
+import tar
+import uuid
+import argparse
+import celery_worker
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('folder')
+parser.add_argument('entrypoint')
+args = parser.parse_args()
+
+tasks = []
+
+for folder in next(os.walk(args.folder))[1]:
+    f = tar.make_tar_file(args.folder, mode='rb')
+    tar_base64 = tar.bin_to_b64(f.read())
+    task_id = str(uuid.uuid4())
+    task = celery_worker.execute.delay(task_id, tar_base64, args.entrypoint)
+    tasks.append(task)
+
+for task in tasks:
+    print(task.get())
