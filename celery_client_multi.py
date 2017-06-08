8938b0293f773c34dd6ae379a9079797c6cd0e5a
AV <netnoc@gmail.com>
2017-06-08 16:38:09 +0200

    Changes in client multi

--- celery_client_multi.py
+++ celery_client_multi.py
@@ -15,9 +15,9 @@ args = parser.parse_args()
 tasks = []
 
 for folder in next(os.walk(args.folder))[1]:
-    f = tar.make_tar_file(args.folder, mode='rb')
+    f = tar.make_tar_file(folder, mode='rb')
     tar_base64 = tar.bin_to_b64(f.read())
-    task_id = str(uuid.uuid4())
+    task_id = folder + ' ' + str(uuid.uuid4())
     task = celery_worker.execute.delay(task_id, tar_base64, args.entrypoint)
     tasks.append(task)
 
